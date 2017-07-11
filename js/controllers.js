b51c1a1191bf30b9ba1be8e245c09cc732c84d98
Christophe <Administrateur@CLIDEV6>
2017-07-11 11:20:53 +0200

    V4.2

--- js/controllers.js
+++ js/controllers.js
@@ -20,6 +20,7 @@ app.controller('AppCtrl', function ( $scope, $http) {
   $scope.rmq =""
   $scope.dat =""
   $scope.der =""
+  $scope.date =""
 
   function get_CT()
   {
@@ -87,5 +88,5 @@ function Ctrl($scope)
 {
   $scope.date = new Date();
 }
-  
+
 });
